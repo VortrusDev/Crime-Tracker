1cad14b40141fcd544e3e3fd6662e5d1ecf5cab2
NicoleBruce <38410637+NicoleBruce@users.noreply.github.com>
2018-10-06 22:48:02 -0400

    Added more things to the .xml files, created base main screen. Attempting to begin adding functionality

---
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledev.crimetrackerapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_main_screen);
+    }
+}
--- app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,10 +14,8 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
