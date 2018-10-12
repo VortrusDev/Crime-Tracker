256ec01ac4a174ddcc818587b97e5b9bff8deb2f
NicoleBruce <38410637+NicoleBruce@users.noreply.github.com>
2018-10-12 10:05:25 -0400

    fixed bug

--- app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
@@ -15,15 +15,6 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
