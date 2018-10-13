35e4384f6c07ced3c70f3b1f7dcd79a7b2e34725
NicoleBruce <38410637+NicoleBruce@users.noreply.github.com>
2018-10-13 10:27:57 -0400

    added functionality to main screen (SETTINGS LEFT ALONE FOR NOW)

--- app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/MainScreenActivity.java
@@ -1,8 +1,11 @@
 package edu.floridapoly.mobiledev.crimetrackerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -10,6 +13,27 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        TextView activities_text = (TextView) findViewById(R.id.home_activities);
+        activities_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreenActivity.this, ActivityListActivity.class));
+            }
+        });
+
+        TextView map_text = (TextView) findViewById(R.id.home_map);
+        map_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreenActivity.this, MapActivity.class));
+            }
+        });
+
+        TextView settings_text = (TextView) findViewById(R.id.home_settings);
+        settings_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainScreenActivity.this, SettingsActivity.class));
+            }
+        });
     }
 
     @Override
@@ -18,6 +42,4 @@ public class MainScreenActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main_screen, menu);
         return true;
     }
-
-
 }
