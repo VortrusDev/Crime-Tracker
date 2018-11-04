5b05a8c458927b1000c97c83f9a1086161ef5a3a
NicoleBruce <38410637+NicoleBruce@users.noreply.github.com>
2018-11-03 22:34:52 -0400

    added settings screen (+fragments) layout files.

--- app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/SettingsActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/SettingsActivity.java
@@ -1,4 +1,14 @@
 package edu.floridapoly.mobiledev.crimetrackerapp;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_screen);
+    }
 }
