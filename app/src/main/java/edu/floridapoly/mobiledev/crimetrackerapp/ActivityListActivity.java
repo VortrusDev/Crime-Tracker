2661e0261fb6312cc3403d008699c2180406eaab
NicoleBruce <38410637+NicoleBruce@users.noreply.github.com>
2018-11-03 21:24:03 -0400

    Made changes to text size, color, and padding in table. Also adjusted column names + removed severity

--- app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/ActivityListActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledev/crimetrackerapp/ActivityListActivity.java
@@ -13,18 +13,18 @@ public class ActivityListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_screen);
-        AddCrimeToTable("crash","Lakeland","low","12 am");// test data
-        AddCrimeToTable("robbery","Orlando","high","6 pm");
-        AddCrimeToTable("fire","Tampa","Medium","OnGoing");
-        AddCrimeToTable("crash","Lakeland","low","12 am");
-        AddCrimeToTable("robbery","Orlando","high","6 pm");
-        AddCrimeToTable("fire","Tampa","Medium","OnGoing");
-        AddCrimeToTable("crash","Lakeland","low","12 am");
-        AddCrimeToTable("robbery","Orlando","high","6 pm");
-        AddCrimeToTable("fire","Tampa","Medium","OnGoing");
+        AddCrimeToTable("crash","Lakeland","10/1/18");// test data
+        AddCrimeToTable("robbery","Orlando","10/1/18");
+        AddCrimeToTable("fire","Tampa","10/2/18");
+        AddCrimeToTable("crash","Lakeland","10/2/18");
+        AddCrimeToTable("robbery","Orlando","10/2/18");
+        AddCrimeToTable("fire","Tampa","10/3/18");
+        AddCrimeToTable("crash","Lakeland","10/4/18");
+        AddCrimeToTable("robbery","Orlando","10/6/18");
+        AddCrimeToTable("fire","Tampa","10/10/18");
     }
     
-    public void AddCrimeToTable(String type,String location,String severity,String time){
+    public void AddCrimeToTable(String type,String location,String date){
         //Method adds crime to table called "crimeTableList"
         // any question regarding this code please ask Daniel
         TableLayout table = (TableLayout) findViewById(R.id.crimeTableList);
@@ -36,24 +36,22 @@ public class ActivityListActivity extends AppCompatActivity {
         ////////////////////////
         if(!tableInitialized){
             TextView header1 = new TextView(this);
-            header1.setText("Type");
+            header1.setTextColor((getResources().getColor(((R.color.secondary_color)))));
+            header1.setText("Activity");
             TextView header2 = new TextView(this);
+            header2.setTextColor((getResources().getColor(((R.color.secondary_color)))));
             header2.setText("Location");
-            TextView header3 = new TextView(this);
-            header3.setText("Severity");
             TextView header4 = new TextView(this);
-            header4.setText("Time");
+            header4.setTextColor((getResources().getColor(((R.color.secondary_color)))));
+            header4.setText("Date");
             // --> formating
-            header1.setTextSize(20);
-            header2.setTextSize(20);
-            header3.setTextSize(20);
-            header4.setTextSize(20);
+            header1.setTextSize(30);
+            header2.setTextSize(30);
+            header4.setTextSize(30);
             header1.setPadding(0,0,60,0);
             header2.setPadding(0,0,60,0);
-            header3.setPadding(0,0,60,0);
             headerRow.addView(header1);
             headerRow.addView(header2);
-            headerRow.addView(header3);
             headerRow.addView(header4);
             headerRow.setPadding(0,0,0,10);
             headerRow.setBackgroundResource(R.drawable.crime_table_border);
@@ -62,16 +60,16 @@ public class ActivityListActivity extends AppCompatActivity {
         }
         /////////////////////////
         TextView test1 = new TextView(this);
+        test1.setTextColor(getResources().getColor((R.color.white_text_color)));
         test1.setText(type);
         TextView test2 = new TextView(this);
+        test2.setTextColor(getResources().getColor((R.color.white_text_color)));
         test2.setText(location);
-        TextView test3 = new TextView(this);
-        test3.setText(severity);
         TextView test4 = new TextView(this);
-        test4.setText(time);
+        test4.setTextColor(getResources().getColor((R.color.white_text_color)));
+        test4.setText(date);
         row.addView(test1);
         row.addView(test2);
-        row.addView(test3);
         row.addView(test4);
         //////////////////////
         row.setBackgroundResource(R.drawable.crime_table_border);
